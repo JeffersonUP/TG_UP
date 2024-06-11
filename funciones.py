f315d44d41754b54813278a95bb8119e197b0dd5
Jefferson <matdor10@gmail.com>
2024-06-11 14:48:54 -0500

    lector de datos

---
+++ funciones.py
@@ -0,0 +1,73 @@
+import pandas as pd
+
+def validar_listado(listado):
+    validos = []
+    no_validos = []
+    df = pd.read_excel("dataframe2.xlsx")
+    df_busqueda = df[df['Equipo'].isin(listado)]
+    for puerto in listado:
+        if df['Equipo'].isin([puerto]).any():
+            validos.append(puerto)
+        else:
+            no_validos.append(puerto)
+    return validos, no_validos
+
+def generar_rangos(listado):
+    lista_switches = []
+    lista_rangos = []
+    df = pd.read_excel("dataframe2.xlsx")
+    df_busqueda = df[df['Equipo'].isin(listado)]
+    editables_pisos = df_busqueda["Piso"].unique()
+    editables_pisos.sort()
+    for piso in editables_pisos:
+        df_piso = df_busqueda[df_busqueda["Piso"] == piso]
+        editables_cuartos = df_piso["Cuarto"].unique()
+        editables_cuartos.sort()
+        #print("piso", piso, end=" ")
+        for cuarto in editables_cuartos:
+            df_cuarto = df_piso[df_piso["Cuarto"] == cuarto]
+            editables_Switches = df_cuarto["Switch"].unique()
+            editables_Switches.sort()
+            # print("cuarto", cuarto, end=" ")
+            for switch in editables_Switches:
+                df_switch = df_cuarto[df_cuarto["Switch"] == switch].sort_values(by="Puerto")
+                editables_puertos = df_switch["Puerto"].unique()
+                editables_puertos.sort()
+                lista_switches.append(f"Piso {piso}, Cuarto {cuarto}, switch {switch}")
+                intervalos = []
+                inicio = editables_puertos[0]
+                fin = editables_puertos[0]
+                for i in range(1, len(editables_puertos)):
+                    if editables_puertos[i] == fin + 1:
+                        fin = editables_puertos[i]
+                    else:
+                        if inicio == fin:
+                            intervalos.append(str(inicio))
+                        else:
+                            intervalos.append(f"{inicio}-{fin}")
+                        inicio = editables_puertos[i]
+                        fin = editables_puertos[i]
+
+                if inicio == fin:
+                    intervalos.append(str(inicio))
+                else:
+                    intervalos.append(f"{inicio}-{fin}")
+               #print(intervalos)
+                if len(editables_puertos) == 1:
+                    interface_range = "int "
+                else:
+                    interface_range = "int range "
+                cont = 0
+                for i in range(0, len(intervalos)):
+                    interface_range += f"g1/0/{intervalos[
+                        i]}"
+                    if i + 1 < len(intervalos):
+                        interface_range += " , "
+                    cont += 1
+                    if cont == 7:
+                        print(interface_range)
+                        interface_range = "int range "
+                        cont = 0
+                if cont != 0:
+                    lista_rangos.append(interface_range)
+    return lista_switches, lista_rangos
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import tkinter
 import tkinter as tk
 from tkinter import ttk
-
-
+from tkinter import messagebox
+import funciones as fn
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -11,20 +11,24 @@ class App(tk.Tk):
         # Crear un Notebook (pestañas)
         self.notebook = ttk.Notebook(self)
         self.notebook.pack(fill='both', expand=True)
-        self.geometry("760x400")
-
+        self.geometry("980x600")
+        self.resizable(False, False)
         # Variables para almacenar datos
         self.var1 = tk.StringVar()
         self.var2 = tk.StringVar()
         self.var3 = tk.StringVar()
-
+        self.lista_equipos = []
         # Crear las pestañas
         self.create_tab1()
         self.create_tab2()
         self.create_tab3()
-
+        self.lista_equipos = []
+        self.lista_switches = []
         self.rowconfigure(0, weight=1)
         self.rowconfigure(1, weight=1)
+        self.rowconfigure(2, weight=1)
+
+        #self.generate_textboxes()
     def create_tab1(self):
         frame1 = tk.Frame(self.notebook)
         self.notebook.add(frame1, text='Configuración')
@@ -33,60 +37,84 @@ class App(tk.Tk):
         frame_columna1 = tk.Frame(frame1)
         frame_columna1.grid(row=0, column=0, sticky="nsew")
 
-        texto_buscar = tk.Text(frame_columna1, width=10, wrap="word")
-        texto_buscar.grid(row=0, column=0, sticky="nsew")
+        self.texto_buscar = tk.Text(frame_columna1, width=10, wrap="word")
+        self.texto_buscar.grid(row=0, column=0, sticky="nsew")
 
-        scroll_buscar = tk.Scrollbar(frame_columna1, orient="vertical", command=texto_buscar.yview)
+        scroll_buscar = tk.Scrollbar(frame_columna1, orient="vertical", command=self.texto_buscar.yview)
         scroll_buscar.grid(row=0, column=1, sticky="ns")
-        texto_buscar.config(yscrollcommand=scroll_buscar.set)
+        self.texto_buscar.config(yscrollcommand=scroll_buscar.set)
 
-        boton_buscar = tkinter.Button(frame_columna1, text="Buscar", command=self.accion_btn_buscar())
+        boton_buscar = tkinter.Button(frame_columna1, text="Buscar", command=self.accion_btn_buscar)
         boton_buscar.grid(row=1, column=0, sticky="ew")
 
         frame1.grid_rowconfigure(0, weight=1)
         frame_columna1.grid_rowconfigure(0, weight=1)
 
         #Columna 2
-        frame_columna2 = tk.Frame(frame1)
+        frame_columna2 = tk.Frame(frame1, width=60)
         frame_columna2.grid(row=0, column=1, sticky="nsew")
 
-        label_informacion = tk.Label(frame_columna2, text="\nEquipos consultados: \n\nEquipos no encontrados:\n\nSwitches involucrados:\n", relief="raised")
-        label_informacion.grid(row=0, column=0, pady=1, sticky="nsew")
+        self.label_informacion = tk.Label(frame_columna2, text="\nEquipos:\t__ \n\n"
+                                                            "no encontrados:\t__\n\n"
+                                                            "total switches:\t__ \n", relief="raised")
+        self.label_informacion.grid(row=0, column=0, padx=3, pady=1, sticky="nsew")
 
         frame_botones = tk.Frame(frame_columna2, borderwidth=1, relief="solid")
-        frame_botones.grid(row=1, column=0, pady=7, sticky="ew")
+        frame_botones.grid(row=1, column=0, padx=3, pady=7, sticky="ew")
 
-        boton_op1 = tk.Button(frame_botones, text="Cambiar VLAN", command=self.accion_btn_1())
-        boton_op1.grid(row=0, column=0, padx=1, pady=3, sticky="ew")
+        boton_op1 = tk.Button(frame_botones, text="Cambiar VLAN", command=self.accion_btn_1)
+        boton_op1.grid(row=0, column=0, padx=3, pady=3, sticky="ew")
 
-        boton_op2 = tk.Button(frame_botones, text="Actualizar PortSecurity ", command=self.accion_btn_2())
-        boton_op2.grid(row=1, column=0, padx=1, pady=3, sticky="ew")
+        boton_op2 = tk.Button(frame_botones, text="Actualizar PortSecurity ", command=self.accion_btn_2)
+        boton_op2.grid(row=1, column=0, padx=3, pady=3, sticky="ew")
 
-        boton_op3 = tk.Button(frame_botones, text="Aplicar Cisco ISE", command=self.accion_btn_2())
-        boton_op3.grid(row=2, column=0, padx=1, pady=3, sticky="ew")
+        boton_op3 = tk.Button(frame_botones, text="Aplicar Cisco ISE", command=self.accion_btn_3)
+        boton_op3.grid(row=2, column=0, padx=3, pady=3, sticky="ew")
 
         frame_botones.grid_columnconfigure(0, weight=1)
 
         #Columna 3
-        frame_columna3=tk.Frame(frame1)
+        frame_columna3 = tk.Frame(frame1)
+        frame_columna3.grid(row=0, column=2, sticky="nsew")
 
+        # Frame para las cajas de texto generadas y el scrollbar
+        self.textbox_frame = tk.Frame(frame_columna3)
+        self.textbox_frame.grid(row=0, column=0, sticky="nsew")
+        self.canvas = tk.Canvas(self.textbox_frame)
+        self.scrollbar = ttk.Scrollbar(self.textbox_frame, orient="vertical", command=self.canvas.yview)
+        self.scrollable_frame = ttk.Frame(self.canvas)
 
+        self.scrollable_frame.bind(
+            "<Configure>",
+            lambda e: self.canvas.configure(
+                scrollregion=self.canvas.bbox("all")
+            )
+        )
 
+        self.canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+
+        self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+        frame1.grid_columnconfigure(2, weight=1)
+        frame_columna3.grid_rowconfigure(0, weight=1)
+        frame_columna3.grid_columnconfigure(0, weight=1)
     def create_tab2(self):
         frame2 = ttk.Frame(self.notebook)
         self.notebook.add(frame2, text='Pestaña 2')
 
         label2 = tk.Label(frame2, text="Contenido de la Pestaña 2", font=("Arial", 20))
-        label2.pack(pady=20)
+        label2.pack(padx=5,pady=20)
 
         entry2 = tk.Entry(frame2, textvariable=self.var2)
-        entry2.pack(pady=10)
+        entry2.pack(padx=5,pady=10)
 
         button2 = tk.Button(frame2, text="Guardar", command=self.save_var2)
-        button2.pack(pady=10)
+        button2.pack(padx=5,pady=10)
 
         self.result_label2 = tk.Label(frame2, text="", font=("Arial", 20))
-        self.result_label2.pack(pady=10)
+        self.result_label2.pack(padx=5, pady=10)
 
     def create_tab3(self):
         frame3 = ttk.Frame(self.notebook)
@@ -105,14 +133,35 @@ class App(tk.Tk):
         self.result_label3.pack(pady=10)
 
     def accion_btn_buscar(self):
-        print("Buscar presionado")
+
+        texto = self.texto_buscar.get("1.0", tk.END).strip()
+        if texto:
+            lineas_texto = texto.split('\n')
+            self.var1, self.var2 = fn.validar_listado(lineas_texto)
+            self.actualizar_texto_buscar()
+            self.generate_textboxes()
+            self.label_informacion.config(text=f"\nTotal Equipos:\t{len(self.var1)} \n\n"
+                                          f"Total Switches:\t{len(self.lista_switches)}\n\n"
+                                          f"No encontrados:\t{len(self.var2)}\n")
+        else:
+            messagebox.showwarning("Entrada vacía", "Por favor, ingrese algún texto antes de registrar.")
+
+
+    def actualizar_texto_buscar(self):
+        no_encontrados=""
+        for elemento in self.var2:
+            no_encontrados += f"{elemento}\n"
+        self.lista_switches, self.lista_rangos = fn.generar_rangos(self.var1)
+        self.texto_buscar.delete('1.0', tk.END)
+        self.texto_buscar.insert(tk.END, no_encontrados)
     def accion_btn_1(self):
+
         print("test")
 
     def accion_btn_2(self):
         print("test")
 
-    def frame1_boton3(self):
+    def accion_btn_3(self):
         print("test")
 
     def frame1_boton4(self):
@@ -134,6 +183,27 @@ class App(tk.Tk):
         value = self.var3.get()
         self.result_label3.config(text=value)
 
+    def generate_textboxes(self):
+        for widget in self.scrollable_frame.winfo_children():
+            widget.destroy()
+
+        try:
+            count = int(len(self.lista_switches))
+        except ValueError:
+            count = 0
+
+        for i in range(count):
+            row_frame = tk.Frame(self.scrollable_frame)
+            row_frame.grid(row=i, column=0, sticky="ew", padx=5, pady=5)
+            titulo = tk.Label(row_frame, text=self.lista_switches[i])
+            titulo.grid(row=0, column=0, columnspan=2, padx=3, pady=1, sticky="w")
+            entry = tk.Text(row_frame, wrap=tk.WORD, height=6)
+            entry.insert(tk.END, self.lista_rangos[i])
+            entry.configure(state='disabled')
+            entry.grid(row=1, column=0, sticky="ew")
+
+            button = tk.Button(row_frame, text="Copiar")
+            button.grid(row=1, column=1, padx=5, sticky="nsew")
 
 if __name__ == "__main__":
     app = App()
